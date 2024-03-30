bc2d9b8d9ff5ee9ffbb08a98e1cd3c5efdbe0010
loganathanspr <loganathanspr@gmail.com>
2024-03-30 13:00:28 +0100

    YT content till 1:20hrs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
+import styles from "./home.module.css"
+
 const Home = () => {
-  return <div>Hello World!</div>;
+  //throw new Error("Error in Home");
+  return <div className={styles.container}>
+    <div className={styles.textContainer}>
+      <h1 className={styles.title}>Creative Thoughts Agency</h1>
+      <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat expedita officiis ipsum repudiandae ratione, enim quisquam soluta dolorem, minima libero molestias repellendus voluptatem id, consequatur vel. Omnis odit eos nam?</p>
+      <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+      </div>
+      <div className={styles.brands}>
+        <Image src="/brands.png" alt="" fill className={styles.brandImg}/>
+      </div>
+    </div>
+    <div className={styles.imageContainer}>
+      <Image src="/hero.gif" alt=""  width={500} height={500}  className={styles.heroImg} />
+    </div>
+  </div>;
 };
 
 export default Home;
