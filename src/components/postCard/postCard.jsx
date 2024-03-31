0fe34cd5a583bd22fbbc2d5b8e1dd3087c93eb90
loganathanspr <loganathanspr@gmail.com>
2024-03-31 23:59:23 +0200

    YT video till 3:00h

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -7,14 +7,14 @@ const PostCard = ({post}) => {
         <div className={styles.container}>
             <div className={styles.top}>
                 <div className={styles.imgContainer}>
-                    <Image src="https://images.pexels.com/photos/18111097/pexels-photo-18111097/free-photo-of-sunshades-in-beach.jpeg" alt="" fill className={styles.img} />
+                    <Image src={post.img} alt="" fill className={styles.img} />
                 </div>
                 <span className={styles.date}>30/March/2024</span>
             </div>
             <div className={styles.bottom}>
                 <h1 className={styles.title}>{post.title}</h1>
                 <p className={styles.desc}>{post.body}</p>
-                <Link className={styles.link} href={`/blog/${post.id}`}>READ MORE</Link>
+                <Link className={styles.link} href={`/blog/${post.slug}`}>READ MORE</Link>
             </div>
         </div>
     )
--- src/components/postUser/postUser.jsx
+++ src/components/postUser/postUser.jsx
@@ -1,5 +1,6 @@
 import { getUser } from "@/lib/data";
 import styles from "./postUser.module.css";
+import Image from "next/image";
 
 // Fetch with API
 // const getData = async (userId) => {
@@ -17,8 +18,16 @@ const PostUser = async ({userId}) => {
     const user = await getUser(userId);
     return (
         <div className={styles.container}>
+            <Image className={styles.avatar}
+                        src={user.img ? user.img : "/noavatar.png"} 
+                        alt="" 
+                        width={50} 
+                        height={50} 
+            />
+            <div className={styles.texts}>
             <span className={styles.title}>Author</span>
-            <span className={styles.username}>{user.username}</span>
+            <span className={styles.username}>{user.username}</span>                
+            </div>
         </div>
     )
 }
